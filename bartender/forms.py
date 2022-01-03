bf710111e381b33304f7cd4e80b7b03a3ccead0d
oskarmay <46371006+oskarmay@users.noreply.github.com>
2022-01-03 22:35:43 +0100

    little modification in ingredient template; creating form for drink;

--- bartender/forms.py
+++ bartender/forms.py
@@ -1,10 +1,10 @@
 from django import forms
 
-from core.models import IngredientStorage
+from core.models import IngredientStorage, Drink
 
 
-class IngredientForm(forms.ModelForm):
-    """Digital type creation form"""
+class IngredientStorageForm(forms.ModelForm):
+    """Ingredient Storage creation form"""
 
     class Meta:
         model = IngredientStorage
@@ -27,5 +27,52 @@ class IngredientForm(forms.ModelForm):
             "storage_amount": forms.NumberInput(attrs={"class": "form-control"}),
             "image": forms.ClearableFileInput(
                 attrs={"class": "form-control form-control-sm"}
+            ),
+        }
+
+
+class DrinkForm(forms.ModelForm):
+    """Digital type creation form"""
+
+    class Meta:
+        model = Drink
+        fields = [
+            "name",
+            "type",
+            "complicated",
+            "amount",
+            "price",
+            "image",
+            "preparation_description",
+        ]
+        label = {
+            "name": ("Nazwa"),
+            "type": ("Typ"),
+            "complicated": ("Skomplikowanie"),
+            "amount": ("Ilość"),
+            "price": ("Cena"),
+            "image": ("Zdjęcie"),
+            "preparation_description": ("Opis"),
+        }
+        widgets = {
+            "name": forms.TextInput(attrs={"class": "form-control"}),
+            "type": forms.Select(
+                choices=Drink.Types.choices, attrs={"class": "form-control"}
+            ),
+            "complicated": forms.Select(
+                choices=Drink.ComplicatedLevels.choices, attrs={"class": "form-control"}
+            ),
+            "amount": forms.NumberInput(attrs={"class": "form-control"}),
+            "price": forms.NumberInput(attrs={"class": "form-control"}),
+            "image": forms.ClearableFileInput(
+                attrs={"class": "form-control form-control-sm"}
+            ),
+            "preparation_description": forms.Textarea(
+                attrs={
+                    "class": "form-control",
+                    "rows": 4,
+                    "cols": 1000,
+                    "max-length": 2048,
+                }
             ),
         }
